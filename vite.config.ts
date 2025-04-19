48573bae37e150770f3c346b8d3b394a8bafd3e0
Sam-Otuonye Royal <samotuonyeroyal@gmail.com>
2025-04-19 16:04:19 +0100

    configured the vite config to use the environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),  tailwindcss()],
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      // Make environment variables accessible in the client-side code
+      'process.env': env,
+    },
+  }
 })
